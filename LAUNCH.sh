161455857ffc3713526a278d4261a56ea4b168ca
vmarquet <vincent@xubuntu-vaio.(none)>
2014-01-06 19:54:41 +0100

    renamed compilation.txt and launch.sh

---
+++ LAUNCH.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# if this script does not work, on Linux do: chmod +x launch.sh
+
+# if we want to compile also:
+# javac -cp ./lib/*:. Minigolf.java
+
+java -cp ./lib/*:. Minigolf
+
+exit 0
